bbb7c2684a4e77873a8a70ec9dd274127ec5bf18
Toon Schoenmakers <nighteyes1993@gmail.com>
2014-12-28 11:24:07 +0100

    Added a few extra test cases
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- test.cpp
+++ test.cpp
@@ -26,6 +26,28 @@ TEST(Decode, Salt) {
   ASSERT_EQ(output[0], 123);
 }
 
+TEST(Encode, SaltMinLength) {
+  hashidsxx::Hashids hash("salt", 15);
+  std::string output = hash.encode({1,2,3,4});
+  ASSERT_GE(output.size(), 15);
+  ASSERT_EQ(output, "7P231QiYuXU53Y9");
+}
+
+TEST(Decode, SaltMinLength) {
+  hashidsxx::Hashids hash("salt", 15);
+  std::vector<uint64_t> output = hash.decode("7P231QiYuXU53Y9");
+  ASSERT_EQ(output.size(), 4);
+  ASSERT_EQ(output[0], 1);
+  ASSERT_EQ(output[1], 2);
+  ASSERT_EQ(output[2], 3);
+  ASSERT_EQ(output[3], 4);
+
+  // we should be able to decode shorter hashes with the same salt regardless of the length..
+  output = hash.decode("0lb");
+  ASSERT_EQ(output.size(), 1);
+  ASSERT_EQ(output[0], 123);
+}
+
 int main(int argc, char** argv) {
   ::testing::FLAGS_gtest_death_test_style = "threadsafe";
   ::testing::FLAGS_gtest_shuffle = true;
